f2dabdd9503838f39adef0615b4891b7db8ba416
Brian R. Bondy <netzen@gmail.com>
2019-06-19 15:32:40 -0400

    Remove patching to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -28,8 +28,6 @@ const materialUIDependencies = ['@material-ui/core']
 const reactDepenendencies = dependencies.filter(dep => dep.match(/react/))
 const d3Dependencies = ['c3', 'd3']
 
-const braveGulp = require('./brave/gulp')
-
 const uiDependenciesToBundle = [
   ...materialUIDependencies,
   ...reactDepenendencies,
@@ -397,7 +395,6 @@ gulp.task('zip', gulp.parallel('zip:chrome', 'zip:firefox', 'zip:edge', 'zip:ope
 
 gulp.task('dev',
   gulp.series(
-    braveGulp,
     'clean',
     'dev:scss',
     gulp.parallel(

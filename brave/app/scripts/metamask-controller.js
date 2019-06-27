04298987db0c5683e84bf4a885f83e8d5c7c9917
ryanml <rlanese@asu.edu>
2019-06-26 17:07:20 -0700

    Fixing lint errors

--- brave/app/scripts/metamask-controller.js
+++ brave/app/scripts/metamask-controller.js
@@ -2,9 +2,6 @@ const MetamaskController = require('../../../app/scripts/metamask-controller')
 const nodeify = require('../../../app/scripts/lib/nodeify')
 
 module.exports = class BraveController extends MetamaskController {
-  constructor (opts) {
-    super(opts)
-  }
 
   getApi () {
     const api = super.getApi()

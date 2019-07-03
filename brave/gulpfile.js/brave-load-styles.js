dfff66121744fa4e11f1c6ce85a4e95a5dcfe67b
ryanml <rlanese@asu.edu>
2019-07-02 20:28:01 -0700

    Adding load-brave-styles task

---
+++ brave/gulpfile.js/brave-load-styles.js
@@ -0,0 +1,31 @@
+const glob = require('glob');
+const gulp = require('gulp')
+const replace = require('gulp-replace')
+
+const getImportedPath = (path) => {
+  const mmPath = path.split('brave/')[1]
+  const braveStyleRelative = '../../../../brave/'
+
+  return `@import '${braveStyleRelative}${mmPath}';`
+}
+
+const createBraveLoadStylesTask = () => {
+  const braveScss = 'brave/ui/app/**/*.scss'
+  const appRootScss = 'ui/app/components/app/index.scss'
+
+  const braveImports = glob.sync(braveScss)
+    .map(path => getImportedPath(path))
+
+  gulp.task('load-brave-styles', function () {
+    return gulp.src(appRootScss)
+      .pipe(
+        replace(
+          /@import 'ui-migration-annoucement\/index';/gm,
+          `@import 'ui-migration-annoucement/index'; ${braveImports}`
+        )
+      )
+      .pipe(gulp.dest(file => file.base))
+  })
+}
+
+module.exports = createBraveLoadStylesTask

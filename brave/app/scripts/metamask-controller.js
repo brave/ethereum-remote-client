c24bb345a8ec508f9d876325a7c72ede76dd754b
ryanml <rlanese@asu.edu>
2019-07-10 18:00:52 -0700

    Only opening new tab on dapp request for unitialized wallets

--- brave/app/scripts/metamask-controller.js
+++ brave/app/scripts/metamask-controller.js
@@ -1,7 +1,34 @@
 const MetamaskController = require('../../../app/scripts/metamask-controller')
+const ProviderApprovalController = require('../../../app/scripts/controllers/provider-approval')
 const nodeify = require('../../../app/scripts/lib/nodeify')
 
 module.exports = class BraveController extends MetamaskController {
+
+  constructor (opts) {
+    super(opts)
+
+    this.optsOpenPopup = opts.openPopup
+    this.providerApprovalController = new ProviderApprovalController({
+      closePopup: opts.closePopup,
+      openPopup: this.openPopup.bind(this),
+      keyringController: this.keyringController,
+      preferencesController: this.preferencesController,
+    })
+
+    this.memStore.config.ProviderApprovalController = this.providerApprovalController.store
+    this.memStore.subscribe(this.sendUpdate.bind(this))
+  }
+
+  openPopup () {
+    const prefStore = this.preferencesController.store
+    const hasOnboarded = prefStore.getState().completedOnboarding
+
+    if (hasOnboarded) {
+      this.optsOpenPopup()
+    } else {
+      chrome.tabs.create({ url: 'chrome://wallet/home.html' })
+    }
+  }
 
   getApi () {
     const api = super.getApi()

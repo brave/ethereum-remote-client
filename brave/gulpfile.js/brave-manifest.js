c960b9cc65dfff53077e2922cd9a27cfaade1299
yan <yan@brave.com>
2019-08-08 11:49:32 -0700

    add wasm-eval CSP to manifest
    needed for argon2

--- brave/gulpfile.js/brave-manifest.js
+++ brave/gulpfile.js/brave-manifest.js
@@ -13,6 +13,9 @@ const createBraveManifestTask = () =>
       json.key = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAl7ZLkqbq8pHRcYANANXmhGKZKHsruBqS0ssf8aI5R5E5FBaWui73FPWMj0g4eggrJeSfF/x/nIIq3Z24Joq1ULuWtheQjbOwqj0yL2ZaxMA6rc5cW2OmGZ0aNXMFclusXftJmwpb/pztHzw55yv8BfJr873HOxtlW2MP1VvSFQjigsbJedlkoS4SKC0U3j8/mjAYR0+lpmBtPitObnYnM5qmtrlg2hgm+sqAon9WKj9nDIjPpXJaCs7kpVl7oQOoYpv47ZT1tnu0o03AL3RZbxPU1N04X3JN6nC+g4gaCgiLkZ+dD79yIt7Il0+vSKuJ+EGySVmv6czOI5eJYezWXwIDAQAB'
       // Version is controlled by brave-core-crx-packager via string replace for 0.0.0
       json.version = '0.0.0'
+      // wasm-eval needed for argon2id password hashing
+      // TODO: lock this down further
+      json.content_security_policy = "script-src 'self' 'wasm-eval'; object-src 'self'"
       return json
     }))
     .pipe(gulp.dest('./dist/brave', { overwrite: true }))

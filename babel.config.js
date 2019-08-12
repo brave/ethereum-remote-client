23255ad9eb200278db8fdaa2a834615c151a490a
Brian R. Bondy <netzen@gmail.com>
2019-08-12 13:56:06 -0400

    Updates for Nodev10 upgrade for MM

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,13 @@ module.exports = function (api) {
       '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-object-rest-spread',
+      [
+        "babel-plugin-root-import",
+        {
+          "braveSuffix": "./",
+          "bravePrefix": "~"
+        }
+      ]
     ],
   }
 }

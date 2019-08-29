8b9935c2d85e38a4ca066cc77ba8e002afae2fbb
yan <yan@brave.com>
2019-08-29 16:38:57 -0700

    allow restoring from metamask
    fix https://github.com/brave/brave-browser/issues/5698
    requires: https://github.com/brave/eth-hd-keyring/pull/4

--- brave/ui/app/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component.js
+++ brave/ui/app/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component.js
@@ -2,8 +2,13 @@ import React from 'react'
 import PasswordWarning from '../password-warning'
 import ImportWithSeedPhrase from '../../../../../../../ui/app/pages/first-time-flow/create-password/import-with-seed-phrase/import-with-seed-phrase.component'
 import { validateMnemonic } from 'bip39'
+import PropTypes from 'prop-types'
 
 module.exports = class BraveImportWithSeedPhrase extends ImportWithSeedPhrase {
+  static contextTypes = {
+    t: PropTypes.func,
+  }
+
   componentDidMount () {
     this.setState({ termsChecked: true })
   }
@@ -13,7 +18,8 @@ module.exports = class BraveImportWithSeedPhrase extends ImportWithSeedPhrase {
 
     if (seedPhrase) {
       const parsedSeedPhrase = this.parseSeedPhrase(seedPhrase)
-      if (parsedSeedPhrase.split(' ').length !== 24) {
+      if (parsedSeedPhrase.split(' ').length !== 24 &&
+        parsedSeedPhrase.split(' ').length !== 12) {
         seedPhraseError = this.context.t('seedPhraseReq')
       } else if (!validateMnemonic(parsedSeedPhrase)) {
         seedPhraseError = this.context.t('invalidSeedPhrase')
@@ -24,9 +30,11 @@ module.exports = class BraveImportWithSeedPhrase extends ImportWithSeedPhrase {
   }
 
   render () {
+    const { t } = this.context
     return (
       <div>
         {super.render()}
+        <div>{t('metamaskImportSubText')}</div>
         <PasswordWarning />
       </div>
     )

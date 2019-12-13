602767686f9853c1d1f2642448968d0742ee51a5
ryanml <rlanese@asu.edu>
2019-12-12 20:04:35 -0700

    Upgrading containers post-rebase

--- brave/ui/app/pages/home/home.container.js
+++ brave/ui/app/pages/home/home.container.js
@@ -3,69 +3,70 @@ import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { unconfirmedTransactionsCountSelector } from '../../../../../ui/app/selectors/confirm-transaction'
-import { getCurrentEthBalance } from '../../../../../ui/app/selectors/selectors'
+import { getCurrentEthBalance, getDaiV1Token, hasPermissionRequests } from '../../../../../ui/app/selectors/selectors'
 import {
-  forceApproveProviderRequestByOrigin,
-  unsetMigratedPrivacyMode,
-  rejectProviderRequestByOrigin,
   addTokens,
   setHardwareConnect,
+  restoreFromThreeBox,
+  turnThreeBoxSyncingOn,
+  getThreeBoxLastUpdated,
+  setShowRestorePromptToFalse,
 } from '../../store/actions'
 import batToken from '../../store/bat-token'
+import { setThreeBoxLastUpdated } from '../../../../../ui/app/ducks/app/app'
 import { getEnvironmentType } from '../../../../../app/scripts/lib/util'
 import { ENVIRONMENT_TYPE_POPUP } from '../../../../../app/scripts/lib/enums'
-
-const activeTabDappProtocols = ['http:', 'https:', 'dweb:', 'ipfs:', 'ipns:', 'ssb:']
 
 const mapStateToProps = state => {
   const { activeTab, metamask, appState } = state
   const {
-    approvedOrigins,
-    dismissedOrigins,
-    lostAccounts,
     suggestedTokens,
-    providerRequests,
-    migratedPrivacyMode,
-    featureFlags: {
-      privacyMode,
-    } = {},
     seedPhraseBackedUp,
     tokens,
+    threeBoxSynced,
+    showRestorePrompt,
+    selectedAddress,
     batTokenAdded,
     hardwareConnect,
   } = metamask
   const accountBalance = getCurrentEthBalance(state)
-  const { forgottenPassword } = appState
+  const { forgottenPassword, threeBoxLastUpdated } = appState
 
-  const isUnconnected = Boolean(
-    activeTab &&
-    activeTabDappProtocols.includes(activeTab.protocol) &&
-    privacyMode &&
-    !approvedOrigins[activeTab.origin] &&
-    !dismissedOrigins[activeTab.origin]
-  )
   const isPopup = getEnvironmentType(window.location.href) === ENVIRONMENT_TYPE_POPUP
 
   return {
-    lostAccounts,
     forgottenPassword,
     suggestedTokens,
     unconfirmedTransactionsCount: unconfirmedTransactionsCountSelector(state),
-    providerRequests,
-    showPrivacyModeNotification: migratedPrivacyMode,
     activeTab,
-    viewingUnconnectedDapp: isUnconnected && isPopup,
     shouldShowSeedPhraseReminder: !seedPhraseBackedUp && (parseInt(accountBalance, 16) > 0 || tokens.length > 0),
     isPopup,
+    threeBoxSynced,
+    showRestorePrompt,
+    selectedAddress,
+    threeBoxLastUpdated,
+    hasDaiV1Token: Boolean(getDaiV1Token(state)),
+    hasPermissionRequests: hasPermissionRequests(state),
     batTokenAdded,
     hardwareConnect,
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  unsetMigratedPrivacyMode: () => dispatch(unsetMigratedPrivacyMode()),
-  forceApproveProviderRequestByOrigin: (origin) => dispatch(forceApproveProviderRequestByOrigin(origin)),
-  rejectProviderRequestByOrigin: origin => dispatch(rejectProviderRequestByOrigin(origin)),
+  turnThreeBoxSyncingOn: () => dispatch(turnThreeBoxSyncingOn()),
+  setupThreeBox: () => {
+    dispatch(getThreeBoxLastUpdated())
+      .then(lastUpdated => {
+        if (lastUpdated) {
+          dispatch(setThreeBoxLastUpdated(lastUpdated))
+        } else {
+          dispatch(setShowRestorePromptToFalse())
+          dispatch(turnThreeBoxSyncingOn())
+        }
+      })
+  },
+  restoreFromThreeBox: (address) => dispatch(restoreFromThreeBox(address)),
+  setShowRestorePromptToFalse: () => dispatch(setShowRestorePromptToFalse()),
   addBatToken: () => dispatch(addTokens(batToken)),
   setHardwareConnect: (value) => dispatch(setHardwareConnect(value)),
 })
--- brave/ui/app/pages/routes/index.js
+++ brave/ui/app/pages/routes/index.js
@@ -7,9 +7,10 @@ import { compose } from 'recompose'
 import { withRouter } from 'react-router-dom'
 
 import {
-  getMetaMaskAccounts,
   getNetworkIdentifier,
   preferencesSelector,
+  hasPermissionRequests,
+  getAddressConnectedToCurrentTab,
 } from '../../../../../ui/app/selectors/selectors'
 import BraveHeader from '../../components/app/header'
 import {
@@ -19,9 +20,8 @@ import {
 import actions from '../../store/actions'
 
 function mapStateToProps (state) {
-  const { appState, metamask } = state
+  const { appState } = state
   const {
-    networkDropdownOpen,
     sidebar,
     alertOpen,
     alertMessage,
@@ -29,84 +29,43 @@ function mapStateToProps (state) {
     loadingMessage,
   } = appState
 
-  const accounts = getMetaMaskAccounts(state)
   const { autoLogoutTimeLimit = 0 } = preferencesSelector(state)
-
-  const {
-    identities,
-    address,
-    keyrings,
-    isInitialized,
-    seedWords,
-    unapprovedTxs,
-    lostAccounts,
-    unapprovedMsgCount,
-    unapprovedPersonalMsgCount,
-    unapprovedTypedMessagesCount,
-    providerRequests,
-    batTokenAdded,
-    rewardsDisclosureAccepted,
-    hardwareConnect,
-  } = metamask
-  const selected = address || Object.keys(accounts)[0]
 
   return {
     // state from plugin
-    networkDropdownOpen,
     sidebar,
     alertOpen,
     alertMessage,
+    textDirection: state.metamask.textDirection,
     isLoading,
     loadingMessage,
-    isInitialized,
     isUnlocked: state.metamask.isUnlocked,
-    selectedAddress: state.metamask.selectedAddress,
     currentView: state.appState.currentView,
-    activeAddress: state.appState.activeAddress,
-    transForward: state.appState.transForward,
-    isOnboarding: Boolean(seedWords || !isInitialized),
-    isPopup: state.metamask.isPopup,
-    seedWords: state.metamask.seedWords,
     submittedPendingTransactions: submittedPendingTransactionsSelector(state),
-    unapprovedTxs,
-    unapprovedMsgs: state.metamask.unapprovedMsgs,
-    unapprovedMsgCount,
-    unapprovedPersonalMsgCount,
-    unapprovedTypedMessagesCount,
-    menuOpen: state.appState.menuOpen,
     network: state.metamask.network,
     provider: state.metamask.provider,
-    forgottenPassword: state.appState.forgottenPassword,
-    lostAccounts,
+    selectedAddress: state.metamask.selectedAddress,
     frequentRpcListDetail: state.metamask.frequentRpcListDetail || [],
     currentCurrency: state.metamask.currentCurrency,
     isMouseUser: state.appState.isMouseUser,
-    isRevealingSeedWords: state.metamask.isRevealingSeedWords,
-    Qr: state.appState.Qr,
-    welcomeScreenSeen: state.metamask.welcomeScreenSeen,
     providerId: getNetworkIdentifier(state),
     autoLogoutTimeLimit,
-
-    identities,
-    selected,
-    keyrings,
-    providerRequests,
-    batTokenAdded,
-    rewardsDisclosureAccepted,
-    hardwareConnect,
+    hasPermissionsRequests: hasPermissionRequests(state),
+    addressConnectedToCurrentTab: getAddressConnectedToCurrentTab(state),
+    batTokenAdded: state.metamask.batTokenAdded,
+    rewardsDisclosureAccepted: state.metamask.rewardsDisclosureAccepted,
+    hardwareConnect: state.metamask.hardwareConnect,
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    dispatch,
+    lockMetaMask: () => dispatch(actions.lockMetamask(false)),
     hideSidebar: () => dispatch(actions.hideSidebar()),
-    showNetworkDropdown: () => dispatch(actions.showNetworkDropdown()),
-    hideNetworkDropdown: () => dispatch(actions.hideNetworkDropdown()),
     setCurrentCurrencyToUSD: () => dispatch(actions.setCurrentCurrency('usd')),
-    toggleAccountMenu: () => dispatch(actions.toggleAccountMenu()),
     setMouseUserState: (isMouseUser) => dispatch(actions.setMouseUserState(isMouseUser)),
     setLastActiveTime: () => dispatch(actions.setLastActiveTime()),
+    showAccountDetail: address => dispatch(actions.showAccountDetail(address)),
   }
 }
 

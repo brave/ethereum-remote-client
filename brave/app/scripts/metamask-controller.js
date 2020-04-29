8fd8aed66ad5dc85c06cce9e086304cd6b04eac9
ryanml <rlanese@asu.edu>
2020-04-29 10:45:57 -0700

    pinning sanitize-html@1.13.0, initializing controller

--- brave/app/scripts/metamask-controller.js
+++ brave/app/scripts/metamask-controller.js
@@ -1,7 +1,37 @@
 const MetamaskController = require('../../../app/scripts/metamask-controller')
+const BitGoController = require('./controllers/bitgo')
 const nodeify = require('../../../app/scripts/lib/nodeify')
 
 module.exports = class BraveController extends MetamaskController {
+
+  constructor (opts) {
+    super(opts)
+
+    if (this.keyringController.password) {
+      this.createBitGoController()
+    }
+  }
+
+  createBitGoController () {
+    chrome.braveWallet.getProjectId((braveProjectId) => {
+      this.bitGoController = new BitGoController({
+        ...this.opts,
+        keyringController: this.keyringController,
+        password: this.keyRingController.password,
+        projectId: braveProjectId
+      })
+    })
+  }
+
+  async createNewVaultAndKeychain (password) {
+    await super.createNewVaultAndKeychain(password)
+    this.createBitGoController()
+  }
+
+  async createNewVaultAndRestore (password) {
+    await super.createNewVaultAndRestore(password)
+    this.createBitGoController()
+  }
 
   getApi () {
     const api = super.getApi()

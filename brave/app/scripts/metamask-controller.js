3d10bd0b7eccc5ae83a9ff470e0e4be8603de944
ryanml <rlanese@asu.edu>
2020-05-12 10:43:25 -0700

    Calling ready function when MetaMaskController is initialized

--- brave/app/scripts/metamask-controller.js
+++ brave/app/scripts/metamask-controller.js
@@ -3,6 +3,14 @@ const nodeify = require('../../../app/scripts/lib/nodeify')
 
 module.exports = class BraveController extends MetamaskController {
 
+  constructor (opts) {
+    super(opts)
+
+    if (chrome.braveWallet.hasOwnProperty('ready')) {
+      chrome.braveWallet.ready()
+    }
+  }
+
   getApi () {
     const api = super.getApi()
     api.setBatTokenAdded = nodeify(this.preferencesController.setBatTokenAdded, this.preferencesController)

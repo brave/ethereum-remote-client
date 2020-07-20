dd17de2d27ec126f4eda359d4568dce8d8e57d91
Brian R. Bondy <netzen@gmail.com>
2020-07-20 16:32:57 -0400

    Show browser action

--- brave/lib/extensionizer/index.js
+++ brave/lib/extensionizer/index.js
@@ -1,10 +1,5 @@
 const extension = require('extensionizer')
 
-extension.browserAction = {
-  setBadgeText: (_opts) => { /* no-op */ },
-  setBadgeBackgroundColor: (_opts) => { /* no-op */ },
-}
-
 extension.runtime.onInstalled = {
   addListener: (_reason) => { /* no-op */ },
 }

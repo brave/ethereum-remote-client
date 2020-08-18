d58dd4cc20197db88e712ea7273dad761db8a4b4
ryanml <rlanese@brave.com>
2020-08-18 12:26:04 -0700

    wip 3

---
+++ brave/build/publish.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const jsoneditor = require('gulp-json-editor')
+const { createTask } = require('../../development/build/task')
+
+function createBravePublishTasks () {
+  const bravePublishTask = createTask('brave-publish', function () {
+    return gulp.src('./package.json')
+      .pipe(jsoneditor(function (json) {
+        delete json.private
+        delete json.browserify
+        json.devDependencies = {}
+        json.dependencies = {}
+        json.scripts = {}
+        return json
+      }))
+      .pipe(gulp.dest('./dist/brave', { overwrite: true }))    
+  })
+
+  return bravePublishTask
+}
+
+module.exports = createBravePublishTasks

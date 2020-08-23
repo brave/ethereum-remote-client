0b6985f6dfcae3add77a02c66b9af827e3360d21
ryanml <rlanese@brave.com>
2020-08-22 18:31:08 -0700

    Fixes brave/brave-browser#11356 - redirects to hardware screen

---
+++ brave/components/welcome/welcome.container.js
@@ -0,0 +1,16 @@
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { setHardwareConnect } from '../../../ui/app/store/actions'
+import BraveWelcome from './welcome.component'
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setHardwareConnect: (value) => dispatch(setHardwareConnect(value)),
+  }
+}
+
+export default compose(
+  withRouter,
+  connect(null, mapDispatchToProps)
+)(BraveWelcome)

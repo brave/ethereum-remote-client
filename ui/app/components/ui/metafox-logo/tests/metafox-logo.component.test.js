5c2297e537ce8abbc8edbc195b97cbdd57d17ee2
ryanml <rlanese@brave.com>
2020-09-12 22:15:29 -0700

    Updating existing tests cases with Crypto Wallets behavior

--- ui/app/components/ui/metafox-logo/tests/metafox-logo.component.test.js
+++ ui/app/components/ui/metafox-logo/tests/metafox-logo.component.test.js
@@ -5,21 +5,12 @@ import MetaFoxLogo from '..'
 
 describe('MetaFoxLogo', function () {
 
-  it('sets icon height and width to 42 by default', function () {
+  it('MetaFox logo does not render', function () {
     const wrapper = mount(
       <MetaFoxLogo />,
     )
 
-    assert.equal(wrapper.find('img.app-header__metafox-logo--icon').prop('width'), 42)
-    assert.equal(wrapper.find('img.app-header__metafox-logo--icon').prop('height'), 42)
-  })
-
-  it('does not set icon height and width when unsetIconHeight is true', function () {
-    const wrapper = mount(
-      <MetaFoxLogo unsetIconHeight />,
-    )
-
-    assert.equal(wrapper.find('img.app-header__metafox-logo--icon').prop('width'), null)
-    assert.equal(wrapper.find('img.app-header__metafox-logo--icon').prop('height'), null)
+    assert.equal(wrapper.find('img.app-header__metafox-logo--icon').length, 0)
+    assert.equal(wrapper.find('img.app-header__metafox-logo--icon').length, 0)
   })
 })

082bf4adddd2031cb5498b0c0332c7c0f144dde3
Marshall Culpepper <mcu@brave.com>
2020-10-16 16:38:09 -0500

    Initial Brave Wallet dropdown selector and header
    Co-authored-by: ryanml <rlanese@brave.com>

---
+++ brave/ui/app/components/app/dropdowns/brave-dropdown-header.component.js
@@ -0,0 +1,58 @@
+import React, { PureComponent } from 'react'
+
+import styles from './assets/styles'
+import PlusIcon from './assets/plus-icon'
+import CaratDownIcon from './assets/carat-down'
+
+module.exports = class BraveDropdownHeader extends PureComponent {
+
+  render () {
+    const {
+      active,
+      isConnect,
+      title,
+      type,
+      onClick,
+      onMouseEnter,
+      onMouseLeave
+    } = this.props
+
+    const activeKey = active ? 'active' : 'inactive'
+
+    return (
+      <div
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        className={`${type}-dropdown`}
+        style={
+          {
+            ...styles.container.general,
+            ...styles.container[activeKey]
+          }
+        }
+      >
+        <span
+          style={
+              {
+              ...styles.title.general,
+              ...styles.title[activeKey]
+              }
+          }
+        >
+          {
+            isConnect
+            ? <div style={styles.plusContainer}>
+                <PlusIcon />
+              </div>
+            : <div style={styles.leftPadding}></div>
+          }
+          {title}
+          <div style={styles.caratContainer}>
+            <CaratDownIcon />
+          </div>
+        </span>
+      </div>
+    )
+  }
+}
---
+++ brave/ui/app/components/app/dropdowns/components/brave-wallet-dropdown/assets/carat-down.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+
+module.exports = class CaratDownIcon extends PureComponent {
+  render () {
+    return (
+      <svg width='10px' height='6px' viewBox='0 0 10 6' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+        <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
+          <g transform='translate(-649.000000, -192.000000)' fillRule='nonzero' stroke='#AEB1C2'>
+            <g transform='translate(46.000000, 176.000000)'>
+              <g transform='translate(456.000000, 0.000000)'>
+                <path d='M151.04767,18.9999941 L153.922598,22.4499077 C154.088331,22.6487875 154.06146,22.9443647 153.862581,23.1100978 C153.663701,23.275831 153.368124,23.2489604 153.20239,23.0500806 L150.07739,19.3000806 C149.932529,19.1262465 149.932529,18.8737417 150.07739,18.6999077 L153.20239,14.9499077 C153.368124,14.7510279 153.663701,14.7241572 153.862581,14.8898904 C154.06146,15.0556236 154.088331,15.3512008 153.922598,15.5500806 L151.04767,18.9999941 Z' transform='translate(152.000000, 18.999994) rotate(-90.000000) translate(-152.000000, -18.999994)'></path>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+    )
+  }
+}

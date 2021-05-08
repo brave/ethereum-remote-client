1bed99c23d27ead3a0ead3471858b8855e183265
Anirudha Bose <anirudha.bose@alumni.cern>
2021-05-08 15:09:08 +0530

    feat(swap): fix title padding and cleanup header panel

--- ui/app/pages/swap/swap-header/swap-header.container.js
+++ ui/app/pages/swap/swap-header/swap-header.container.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux'
 import { clearSwap } from '../../../store/actions'
 import SwapHeader from './swap-header.component'
-import { getSwapTitleKey } from '../../../selectors'
 import { getMostRecentOverviewPage } from '../../../ducks/history/history'
 
 export default connect(mapStateToProps, mapDispatchToProps)(SwapHeader)
@@ -9,7 +8,6 @@ export default connect(mapStateToProps, mapDispatchToProps)(SwapHeader)
 function mapStateToProps (state) {
   return {
     mostRecentOverviewPage: getMostRecentOverviewPage(state),
-    titleKey: getSwapTitleKey(state),
   }
 }
 

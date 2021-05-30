f17693844bc23bda75da3ccbcd768465eea8fef4
Anirudha Bose <anirudha.bose@alumni.cern>
2021-05-30 18:25:32 +0530

    chore(swap): cleanup top-level swap component

--- ui/app/pages/swap/swap.component.js
+++ ui/app/pages/swap/swap.component.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {
-  getAmountErrorObject,
-  getGasFeeErrorObject,
-  getToAddressForGasUpdate,
-  doesAmountErrorRequireUpdate,
-} from './swap.utils'
-import { debounce } from 'lodash'
 import SwapHeader from './swap-header'
 import SwapContent from './swap-content'
 import SwapFooter from './swap-footer'
@@ -14,41 +7,8 @@ import SwapFooter from './swap-footer'
 export default class SwapTransactionScreen extends Component {
 
   static propTypes = {
-    amount: PropTypes.string,
-    blockGasLimit: PropTypes.string,
-    conversionRate: PropTypes.number,
-    editingTransactionId: PropTypes.string,
-    fetchBasicGasEstimates: PropTypes.func.isRequired,
-    from: PropTypes.object,
-    gasLimit: PropTypes.string,
-    gasPrice: PropTypes.string,
-    gasTotal: PropTypes.string,
-    hasHexData: PropTypes.bool,
     history: PropTypes.object,
-    network: PropTypes.string,
-    primaryCurrency: PropTypes.string,
     resetSwapState: PropTypes.func.isRequired,
-    selectedAddress: PropTypes.string,
-    swapFromToken: PropTypes.object,
-    showHexData: PropTypes.bool,
-    to: PropTypes.string,
-    toNickname: PropTypes.string,
-    tokensFrom: PropTypes.array,
-    tokensTo: PropTypes.array,
-    tokenFromBalance: PropTypes.string,
-    tokenFromContract: PropTypes.object,
-    tokenToBalance: PropTypes.string,
-    tokenToContract: PropTypes.object,
-    updateAndSetGasLimit: PropTypes.func.isRequired,
-    updateSwapEnsResolution: PropTypes.func.isRequired,
-    updateSwapEnsResolutionError: PropTypes.func.isRequired,
-    updateSwapErrors: PropTypes.func.isRequired,
-    updateSwapTo: PropTypes.func.isRequired,
-    updateSwapTokenBalance: PropTypes.func.isRequired,
-    updateToNicknameIfNecessary: PropTypes.func.isRequired,
-    scanQrCode: PropTypes.func.isRequired,
-    qrCodeDetected: PropTypes.func.isRequired,
-    qrCodeData: PropTypes.object,
   }
 
   static contextTypes = {
@@ -56,288 +16,20 @@ export default class SwapTransactionScreen extends Component {
     metricsEvent: PropTypes.func,
   }
 
-  state = {
-    query: '',
-    toError: null,
-    toWarning: null,
-  }
-
-  constructor (props) {
-    super(props)
-    this.dValidate = debounce(this.validate, 1000)
-  }
-
-  componentDidUpdate (prevProps) {
-    const {
-      amount,
-      conversionRate,
-      from: { address, balance },
-      gasTotal,
-      network,
-      primaryCurrency,
-      swapFromToken,
-
-      tokenToBalance,
-      tokenFromBalance,
-      updateSwapErrors,
-      updateSwapTo,
-      updateSwapTokenBalance,
-      tokenToContract,
-      to,
-    } = this.props
-
-    let updateGas = false
-    const {
-      from: { balance: prevBalance },
-      gasTotal: prevGasTotal,
-      tokenFromBalance: prevTokenFromBalance,
-      tokenToBalance: prevTokenToBalance,
-      network: prevNetwork,
-      swapFromToken: prevSwapFromToken,
-      to: prevTo,
-    } = prevProps
-
-    const uninitialized = [prevBalance, prevGasTotal].every((n) => n === null)
-
-    const amountErrorRequiresUpdate = doesAmountErrorRequireUpdate({
-      balance,
-      gasTotal,
-      prevBalance,
-      prevGasTotal,
-      prevTokenToBalance,
-      prevTokenFromBalance,
-      swapFromToken,
-
-      tokenToBalance,
-      tokenFromBalance,
-    })
-
-    if (amountErrorRequiresUpdate) {
-      const amountErrorObject = getAmountErrorObject({
-        amount,
-        balance,
-        conversionRate,
-        gasTotal,
-        primaryCurrency,
-        swapFromToken,
-
-        tokenFromBalance,
-        tokenToBalance,
-      })
-      const gasFeeErrorObject = swapFromToken
-        ? getGasFeeErrorObject({
-          balance,
-          conversionRate,
-          gasTotal,
-          primaryCurrency,
-          swapFromToken,
-        })
-        : { gasFee: null }
-      updateSwapErrors(Object.assign(amountErrorObject, gasFeeErrorObject))
-    }
-
-    if (!uninitialized) {
-
-      if (network !== prevNetwork && network !== 'loading') {
-        updateSwapToTokenBalance({
-
-          tokenFromContract,
-          address,
-        })
-      }
-    }
-
-    const prevFromTokenAddress = prevSwapFromToken && prevSwapFromToken.address
-    const swapTokenFromAddress = swapFromToken && swapFromToken.address
-
-    if (swapTokenFromAddress && prevFromTokenAddress !== swapFromTokenAddress) {
-      this.updateSwapFromToken()
-      updateGas = true
-    }
-
-    // const prevToTokenAddress = prevSwapToToken && prevSwapToToken.address
-    // const swapTokenToAddress = swapToToken && swapToToken.address
-    // if (swapTokenToAddress && prevToTokenAddress !== swapToTokenAddress) {
-    //   this.updateSwapToToken()
-    //   updateGas = true
-    // }
-
-    // let scannedAddress
-    // if (qrCodeData) {
-    //   if (qrCodeData.type === 'address') {
-    //     scannedAddress = qrCodeData.values.address.toLowerCase()
-    //     const currentAddress = prevTo && prevTo.toLowerCase()
-    //     if (currentAddress !== scannedAddress) {
-    //       updateSwapTo(scannedAddress)
-    //       updateGas = true
-    //       // Clean up QR code data after handling
-    //       qrCodeDetected(null)
-    //     }
-    //   }
-    // }
-
-    if (updateGas) {
-      if (scannedAddress) {
-        this.updateGas({ to: scannedAddress })
-      } else {
-        this.updateGas()
-      }
-    }
-  }
-
-  componentDidMount () {
-    this.props.fetchBasicGasEstimates()
-      .then(() => {
-        this.updateGas()
-      })
-  }
-
-  UNSAFE_componentWillMount () {
-    this.updateSwapFromToken()
-    this.updateSwapToToken()
-
-    // Show QR Scanner modal  if ?scan=true
-    if (window.location.search === '?scan=true') {
-      this.props.scanQrCode()
-
-      // Clear the queryString param after showing the modal
-      const cleanUrl = window.location.href.split('?')[0]
-      window.history.pushState({}, null, `${cleanUrl}`)
-      window.location.hash = '#swap'
-    }
-  }
-
   componentWillUnmount () {
-    this.props.resetSwapState()
-  }
-
-  onRecipientInputChange = (query) => {
-    if (query) {
-      this.dValidate(query)
-    } else {
-      this.validate(query)
-    }
-
-    this.setState({
-      query,
-    })
-  }
-
-  validate (query) {
-    const {
-      hasHexData,
-      tokensFrom,
-      tokensTo,
-      swapFromToken,
-
-      network,
-    } = this.props
-
-    if (!query) {
-      return this.setState({ toError: '', toWarning: '' })
-    }
-
-
-    // FIXME (@onyb): leftover from ui/app/pages/swap/swap-content/add-recipient
-    //
-    // const toErrorObject = getToErrorObject(query, hasHexData, network)
-    // const toWarningObject = getToWarningObject(query, tokensFrom, swapFromToken, swapToToken)
-    this.setState({
-      toError: '', // toErrorObject.to,
-      toWarning: '', // toWarningObject.to,
-    })
-  }
-
-  updateSwapFromToken () {
-    const {
-      from: { address },
-      swapFromToken,
-      tokenFromContract,
-      updateSwapTokenBalance,
-    } = this.props
-
-    updateSwapTokenBalance({
-      swapFromToken,
-      tokenFromContract,
-      address,
-    })
-  }
-
-  updateSwapToToken () {
-    const {
-      from: { address },
-
-      tokenToContract,
-      updateSwapTokenBalance,
-    } = this.props
-
-    updateSwapTokenBalance({
-
-      tokenToContract,
-      address,
-    })
-  }
-
-  updateGas ({ to: updatedToAddress, amount: value, data } = {}) {
-    const {
-      amount,
-      blockGasLimit,
-      editingTransactionId,
-      gasLimit,
-      gasPrice,
-      selectedAddress,
-      fromAsset,
-      to: currentToAddress,
-      updateAndSetGasLimit,
-    } = this.props
-
-    updateAndSetGasLimit({
-      blockGasLimit,
-      editingTransactionId,
-      gasLimit,
-      gasPrice,
-      selectedAddress,
-      fromAsset,
-
-      to: getToAddressForGasUpdate(updatedToAddress, currentToAddress),
-      value: value || amount,
-      data,
-    })
+    const { resetSwapState } = this.props
+    resetSwapState()
   }
 
   render () {
     const { history } = this.props
-    // TODO: HACK ; to is not been passed here.
-    const to = '0x4F75D92c8BC5CbcD9D6BA5fc0D58A28089E48e37'
-
-    const content = this.renderSwapContent()
-
-    // if (to) {
-    //   content = this.renderSwapContent()
-    // } else {
-    //   content = this.renderAddRecipient()
-    // }
 
     return (
       <div className="page-container">
         <SwapHeader history={history} />
-        {/* { this.renderInput() } */}
-        { content }
+        <SwapContent />
+        <SwapFooter history={history} />
       </div>
     )
   }
-
-  renderSwapContent () {
-    const { history, showHexData } = this.props
-
-    return [
-      <SwapContent
-        key="swap-content"
-        updateGas={({ amount, data } = {}) => this.updateGas({ amount, data })}
-        showHexData={showHexData}
-      />,
-      <SwapFooter key="swap-footer" history={history} />,
-    ]
-  }
-
 }
--- ui/app/pages/swap/swap.container.js
+++ ui/app/pages/swap/swap.container.js
@@ -1,118 +1,18 @@
 import { connect } from 'react-redux'
-import Swap from './swap.component'
 import { withRouter } from 'react-router-dom'
 import { compose } from 'redux'
 
-import {
-  getAddressBook,
-  getQrSwapsCodeData,
-  getSelectedAddress,
-  getSwapAmount,
-  getSwapBlockGasLimit,
-  getSwapConversionRate,
-  getSwapCurrentNetwork,
-  getSwapEditingTransactionId,
-  getSwapFromAsset,
-  getSwapFromObject,
-  getSwapFromTokenBalance,
-  getSwapFromTokenContract,
-  getSwapGasPrice,
-  getSwapHexDataFeatureFlagState,
-  getSwapPrimaryCurrency,
-  getSwapQuoteGasLimit,
-  getSwapTo,
-  getSwapToAsset,
-  getSwapToNickname,
-  getSwapToTokenBalance,
-  getSwapToTokenContract,
-} from '../../selectors'
+import { resetSwapState } from '../../ducks/swap/swap.duck'
+import Swap from './swap.component'
 
-import {
-  qrCodeDetected,
-  setGasTotal,
-  showQrScanner,
-  updateGasData,
-  updateSwapEnsResolution,
-  updateSwapEnsResolutionError,
-  updateSwapFromTokenBalance,
-  updateSwapTo,
-} from '../../store/actions'
-import { resetSwapState, updateSwapErrors } from '../../ducks/swap/swap.duck'
-import { fetchBasicGasEstimates } from '../../ducks/gas/gas.duck'
-import { getTokens } from '../../ducks/metamask/metamask'
-import { calcGasTotal } from './swap.utils.js'
-import { isValidDomainName } from '../../helpers/utils/util'
 
-function mapStateToProps (state) {
-  const editingTransactionId = getSwapEditingTransactionId(state)
-
-  return {
-    addressBook: getAddressBook(state),
-    amount: getSwapAmount(state),
-    blockGasLimit: getSwapBlockGasLimit(state),
-    conversionRate: getSwapConversionRate(state),
-    editingTransactionId,
-    from: getSwapFromObject(state),
-    gasLimit: getSwapQuoteGasLimit(state),
-    gasPrice: getSwapGasPrice(state),
-    gasTotal: '0',
-    network: getSwapCurrentNetwork(state),
-    primaryCurrency: getSwapPrimaryCurrency(state),
-    qrCodeData: getQrSwapsCodeData(state),
-    selectedAddress: getSelectedAddress(state),
-    fromAsset: getSwapFromAsset(state),
-    toAsset: getSwapToAsset(state),
-    showHexData: getSwapHexDataFeatureFlagState(state),
-    to: getSwapTo(state),
-    toNickname: getSwapToNickname(state),
-    tokens: getTokens(state),
-    tokenFromBalance: getSwapFromTokenBalance(state),
-    tokenFromContract: getSwapFromTokenContract(state),
-    tokenToBalance: getSwapToTokenBalance(state),
-    tokenToContract: getSwapToTokenContract(state),
-  }
+function mapStateToProps () {
+  return {}
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    updateAndSetGasLimit: ({
-      blockGasLimit,
-      editingTransactionId,
-      gasLimit,
-      gasPrice,
-      selectedAddress,
-      fromAsset,
-      to,
-      value,
-      data,
-    }) => {
-      !editingTransactionId
-        ? dispatch(updateGasData({ gasPrice, selectedAddress, fromAsset, blockGasLimit, to, value, data }))
-        : dispatch(setGasTotal(calcGasTotal(gasLimit, gasPrice)))
-    },
-    updateSwapTokenBalance: ({ fromAsset, tokenContract, address }) => {
-      dispatch(updateSwapFromTokenBalance({
-        fromAsset,
-        tokenContract,
-        address,
-      }))
-    },
-    updateSwapErrors: (newError) => dispatch(updateSwapErrors(newError)),
     resetSwapState: () => dispatch(resetSwapState()),
-    scanQrCode: () => dispatch(showQrScanner()),
-    qrCodeDetected: (data) => dispatch(qrCodeDetected(data)),
-    updateSwapTo: (to, nickname) => dispatch(updateSwapTo(to, nickname)),
-    fetchBasicGasEstimates: () => dispatch(fetchBasicGasEstimates()),
-    updateSwapEnsResolution: (ensResolution) => dispatch(updateSwapEnsResolution(ensResolution)),
-    updateSwapEnsResolutionError: (message) => dispatch(updateSwapEnsResolutionError(message)),
-    updateToNicknameIfNecessary: (to, toNickname, addressBook) => {
-      if (isValidDomainName(toNickname)) {
-        const addressBookEntry = addressBook.find(({ address }) => to === address) || {}
-        if (!addressBookEntry.name !== toNickname) {
-          dispatch(updateSwapTo(to, addressBookEntry.name || ''))
-        }
-      }
-    },
   }
 }
 

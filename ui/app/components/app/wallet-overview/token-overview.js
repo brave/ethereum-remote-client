50013c256f01e05525a7b9688fb02257598e7548
Anirudha Bose <anirudha.bose@alumni.cern>
2021-07-01 11:46:16 +0530

    fix(swap): add missing swap button to token overview page
    Fixes brave/brave-browser#16645.

--- ui/app/components/app/wallet-overview/token-overview.js
+++ ui/app/components/app/wallet-overview/token-overview.js
@@ -8,13 +8,16 @@ import Identicon from '../../ui/identicon'
 import CurrencyDisplay from '../../ui/currency-display'
 import { I18nContext } from '../../../contexts/i18n'
 import WalletOverview from './wallet-overview'
-import { SEND_ROUTE } from '../../../helpers/constants/routes'
+import { SEND_ROUTE, SWAP_ROUTE } from '../../../helpers/constants/routes'
 import { useMetricEvent } from '../../../hooks/useMetricEvent'
 import { useTokenTracker } from '../../../hooks/useTokenTracker'
 import { useTokenFiatAmount } from '../../../hooks/useTokenFiatAmount'
-import { getAssetImages } from '../../../selectors/selectors'
-import { updateSendToken } from '../../../store/actions'
+import { getAssetImages, getNetworkIdentifier } from '../../../selectors/selectors'
+import { updateSendToken, updateSwapFromAsset } from '../../../store/actions'
 import PaperAirplane from '../../ui/icon/paper-airplane-icon'
+import Interaction from '../../ui/icon/interaction-icon.component'
+import Tooltip from '../../ui/tooltip-v2'
+import { MAINNET, ROPSTEN } from '../../../../../app/scripts/controllers/network/enums'
 
 const TokenOverview = ({ className, token }) => {
   const dispatch = useDispatch()
@@ -26,11 +29,21 @@ const TokenOverview = ({ className, token }) => {
       name: 'Clicked Send: Token',
     },
   })
+  const swapEvent = useMetricEvent({
+    eventOpts: {
+      category: 'Navigation',
+      action: 'Home',
+      name: 'Clicked Swap',
+    },
+  })
   const history = useHistory()
   const assetImages = useSelector(getAssetImages)
   const { tokensWithBalances } = useTokenTracker([token])
   const balance = tokensWithBalances[0]?.string
   const formattedFiatBalance = useTokenFiatAmount(token.address, balance, token.symbol)
+  const networkIdentifier = useSelector(getNetworkIdentifier)
+
+  const isSwapAvailable = [MAINNET, ROPSTEN].includes(networkIdentifier)
 
   return (
     <WalletOverview
@@ -55,19 +68,48 @@ const TokenOverview = ({ className, token }) => {
         </div>
       )}
       buttons={(
-        <Button
-          type="secondary"
-          className="token-overview__button"
-          rounded
-          icon={<PaperAirplane color="#037DD6" size={20} />}
-          onClick={() => {
-            sendTokenEvent()
-            dispatch(updateSendToken(token))
-            history.push(SEND_ROUTE)
-          }}
-        >
-          { t('send') }
-        </Button>
+        <>
+          <Button
+            type="secondary"
+            className="token-overview__button"
+            rounded
+            icon={<PaperAirplane color="#037DD6" size={20} />}
+            onClick={() => {
+              sendTokenEvent()
+              dispatch(updateSendToken(token))
+              history.push(SEND_ROUTE)
+            }}
+          >
+            { t('send') }
+          </Button>
+          <Tooltip
+            position="bottom"
+            title={t('availableOnMainnetRopsten')}
+            disabled={isSwapAvailable}
+            distance={-30}
+            style={{
+              position: 'relative',
+              paddingLeft: '90px',
+              top: '-20px',
+            }}
+          >
+            <Button
+              type="secondary"
+              className="token-overview__button"
+              rounded
+              icon={<Interaction color="#037DD6" size={20} />}
+              onClick={() => {
+                dispatch(updateSwapFromAsset(token))
+                swapEvent()
+                history.push(SWAP_ROUTE)
+              }}
+              data-testid="token-overview__button"
+              disabled={!isSwapAvailable}
+            >
+              { t('swap') }
+            </Button>
+          </Tooltip>
+        </>
       )}
       className={className}
       icon={(

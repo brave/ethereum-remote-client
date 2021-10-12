ae9ce8068d098ba9b9cb222a7a166f032f5b135c
Sampson <jjdsampson@gmail.com>
2021-10-12 13:51:13 -0500

    Update phishing-controller.js
    WIP Adds support for consuming Phishfort's hosting provider and domain whitelists. Still needs *.brave.com endpoints to serve the raw JSON.

--- app/scripts/controllers/phishing-controller.js
+++ app/scripts/controllers/phishing-controller.js
@@ -6,22 +6,39 @@ export default class BravePhishingController extends PhishingController {
   constructor (config, state) {
     super(config, state)
     this.configUrl = 'https://mainnet-infura-api.brave.com/blacklist'
+    this.phishfortDomainList = 'https://raw.githubusercontent.com/phishfort/phishfort-lists/master/whitelists/domains.json'
+    this.phishfortHostingList = 'https://raw.githubusercontent.com/phishfort/phishfort-lists/master/whitelists/hostingproviders.json'
     this.phishfortResourceUrl = 'https://mainnet-infura-api.brave.com/phishfort'
   }
 
-  async fetchPhishfortDenyList () {
-    const phishFortDenylist = await fetch(this.phishfortResourceUrl) // eslint-disable-line no-undef
-    return await phishFortDenylist.json()
+  async fetchPhishfortLists () {
+    const phishFortDenyList = await fetch(this.phishfortResourceUrl) // eslint-disable-line no-undef
+    const phishFortDomainList = await fetch(this.phishfortDomainList) // eslint-disable-line no-undef
+    const phishFortHostingList = await fetch(this.phishfortHostingList) // eslint-disable-line no-undef
+    return Promise.all([
+      phishFortDenyList.json(),
+      phishFortDomainList.json(),
+      phishFortHostingList.json(),
+    ])
   }
 
   async update (state, overwrite = false) {
-    if (state.phishing && state.phishing.blacklist) {
-      const denyList = await this.fetchPhishfortDenyList()
-      if (denyList) {
-        state.phishing.blacklist = [
-          ...state.phishing.blacklist,
-          ...denyList,
-        ]
+    if (state.phishing && state.phishing.blacklist && state.phishing.whitelist) {
+      const [denyList, domainList, hostingList] = await this.fetchPhishfortLists()
+      if (denyList || domainList || hostingList) {
+        if (denyList) {
+          state.phishing.blacklist = [
+            ...state.phishing.blacklist,
+            ...denyList,
+          ]
+        }
+        if (hostingList || domainList) {
+          state.phishing.whitelist = [
+            ...state.phishing.whitelist,
+            ...(hostingList || []),
+            ...(domainList || []),
+          ]
+        }
         this.detector = new PhishingDetector(state.phishing)
       }
     }
